8f20b8e1c5847c9d01bb334672f192de035fef4d
dqnx19 <dan.k.19@outlook.com>
2026-05-17 08:55:58 +0200

    added script to import app drawer to nether websites

---
+++ js/import-app-drawer.js
@@ -0,0 +1,17 @@
+document.querySelector(".app-drawer-wrapper").innerHTML = `
+<button onclick="openAppDrawer()" class="app-drawer-button"></button>
+        <div class="app-drawer">
+            <button onclick="openAppDrawer()" class="app-drawer-button"></button>
+            <a href="https://srt.nether.click">
+                <img src="https://srt.nether.click/img/icons/logo.svg" alt="">
+                Sigma Republic Transport
+            </a>
+            <a href="https://srb.nether.click">
+                <img src="https://srb.nether.click/img/icons/logo.svg" alt="">
+                Sigma Republic Bank
+            </a>
+            <a href="https://modern-web.nether.click">
+                Nether Modern Web
+            </a>
+        </div>
+`;
