d53fe5c362e75b7361ebaff6d914539f31d61e9a
dqnx19 <dan.k.19@outlook.com>
2026-05-20 16:47:58 +0200

    updated app drawer

--- js/import-app-drawer.js
+++ js/import-app-drawer.js
@@ -1,17 +1,33 @@
 document.querySelector(".app-drawer-wrapper").innerHTML = `
-<button onclick="openAppDrawer()" class="app-drawer-button"></button>
-        <div class="app-drawer">
-            <button onclick="openAppDrawer()" class="app-drawer-button"></button>
-            <a href="https://srt.nether.click">
-                <img src="https://srt.nether.click/img/icons/logo.svg" alt="">
-                Sigma Republic Transport
-            </a>
-            <a href="https://srb.nether.click">
-                <img src="https://srb.nether.click/img/icons/logo.svg" alt="">
-                Sigma Republic Bank
-            </a>
-            <a href="https://modern-web.nether.click">
-                Nether Modern Web
-            </a>
-        </div>
+    <button onclick="openAppDrawer()" class="app-drawer-button"></button>
+    <div class="app-drawer">
+        <button onclick="openAppDrawer()" class="app-drawer-button"></button>
+        <a href="https://nether.click">
+            Nether
+        </a>
+        <a href="https://modern-web.nether.click">
+            <img src="https://modern-web.nether.click/img/favicon.svg" alt="">
+            Nether Modern Web
+        </a>
+        <a href="https://learn-html.nether.click">
+            <img src="https://learn-html.nether.click/favicon.png" alt="">
+            Nether Learn HTML
+        </a>
+        <a href="https://learn-css.nether.click">
+            <img src="https://learn-css.nether.click/favicon.png" alt="">
+            Nether Learn CSS
+        </a>
+        <a href="https://os.nether.click">
+            <img src="https://os.nether.click/img/logo-small.svg" alt="">
+            NetherOS
+        </a>
+        <a href="https://srt.nether.click">
+            <img src="https://srt.nether.click/img/icons/logo.svg" alt="">
+            Sigma Republic Transport
+        </a>
+        <a href="https://srb.nether.click">
+            <img src="https://srb.nether.click/img/icons/logo.svg" alt="">
+            Sigma Republic Bank
+        </a>
+    </div>
 `;
