9d59e0f4c894fd027fbc804ad9f6c2cdfd19fc67
Dan <dan.k.19@outlook.com>
2026-05-22 10:40:10 +0200

    Updated showourproducts function and showhome with button to our products page

--- pages.js
+++ pages.js
@@ -8,7 +8,7 @@ function showHome() {
         <h1>Home Page</h1>
         <section>
             <h2>Welcome to Nether</h2>
-            <p>You can pick from one of our products on our products page</p>
+            <p>You can pick from one of our products on <button onclick="showOurProducts()" our products page</button></p>
         </section>
     `;
 }
@@ -18,12 +18,23 @@ function showOurProducts() {
     main.innerHTML = `
         <h1>Our Products</h1>
         <section>
+            <h2>Nether</h2>
+            <a href="https://nether.click">Nether</a>
+            <a href="https://modern-web.nether.click">Nether Modern Web</a>
+        </section>
+        <section>
             <h2>NetherOS</h2>
-            <p>Product 1 description</p>
+            <a href="https://os.nether.click">NetherOS</a>
+        </section>
+        <section>
+            <h2>Nether Learn</h2>
+            <a href="https://learn-html.nether.click">Nether Learn HTML</a>
+            <a href="https://learn-css.nether.click">Nether Learn CSS</a>
         </section>
         <section>
             <h2>Sigma Republic</h2>
-            <p>Product 2 description</p>
+            <a href="https://srt.nether.click">Sigma Republic Transport</a>
+            <a href="https://srb.nether.click">Sigma Republic Bank</a>
         </section>
     `;
 }
