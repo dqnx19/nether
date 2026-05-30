5a8c08731e04f04b5bb88354178a519423833ec9
dqnx19 <dan.k.19@outlook.com>
2026-05-30 10:44:56 +0200

    ficed wallpapers

--- js/import-wallpapers.js
+++ js/import-wallpapers.js
@@ -1,4 +1,17 @@
 const day = new Date().getDate();
 
-document.body.style.backgroundImage =
-    `url("https://nether.click/img/wallpaper/${day}.jpg")`;
+document.body.style.backgroundImage = `url("https://nether.click/img/wallpapers/${day}.jpg")`;
+
+const style = document.createElement("style");
+
+style.innerHTML = `
+body {
+            background-image: url("https://nether.click/img/wallpapers/1.jpg");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }
+`;
+
+document.head.appendChild(style);
